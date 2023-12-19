4a0335f28d5abc061d0739e4d7693f91de91fd3c
Aaron Kunde <aaron.kunde@adesso.de>
2023-12-19 15:52:03 +0100

    Add some custom metrics

--- apps/app1/src/main/java/pt/obs/rest/AppRestController.java
+++ apps/app1/src/main/java/pt/obs/rest/AppRestController.java
@@ -1,21 +1,28 @@
 package pt.obs.rest;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
-public class AppRestController {
+class AppRestController {
+
+    private Counter api1Counter = Metrics.counter("app1.api.1.counter", "it-1", "it-2");
+    private Counter api2Counter = Metrics.counter("app1.api.2.counter", "it-1", "it-3");
 
     @GetMapping("/api-1")
     void api1() {
         log.info("Calling API 1");
+        api1Counter.increment();
     }
 
     @GetMapping("/api-2")
     void api2() {
         log.info("Calling API 2");
+        api2Counter.count();
 
         throw new RuntimeException("An unexpected error occurred");
     }
