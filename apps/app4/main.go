effb8df692d981850d5097f02fb64a0fb7a18694
Aaron Kunde <aaron.kunde@adesso.de>
2023-12-26 16:03:04 +0100

    Add HTTP-Handling

--- apps/app4/main.go
+++ apps/app4/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"fmt"
 	log "github.com/sirupsen/logrus"
+	"net/http"
 )
 
 func main()  {
 	log.Info("Starting app4")
+
+	http.HandleFunc("/api-1", func(writer http.ResponseWriter, request *http.Request) {
+		log.Infof("Requeseted API-1:%s", request.URL)
+		// Write response
+		fmt.Fprintf(writer, "Hello, you've requested: %s\n", request.URL.Path)
+	})
+
+	http.ListenAndServe(":8084", nil)
 }
