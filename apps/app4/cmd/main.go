6bba1f804e93fe811e3ae3b12781b66d631e9e91
Aaron Kunde <aaron.kunde@adesso.de>
2023-12-28 15:19:59 +0100

    1st implementation of the 3 APIs

--- apps/app4/cmd/main.go
+++ apps/app4/cmd/main.go
@@ -10,7 +10,25 @@ func main()  {
 	log.Info("Starting app4")
 
 	http.HandleFunc("/api-1", func(writer http.ResponseWriter, request *http.Request) {
+		log.Info("Calling API 1");
+
 		log.Infof("Requeseted API-1:%s", request.URL)
+		// Write response
+		fmt.Fprintf(writer, "Hello, you've requested: %s\n", request.URL.Path)
+	})
+
+	http.HandleFunc("/api-2", func(writer http.ResponseWriter, request *http.Request) {
+		log.Info("Calling API 2");
+		// Must be called, before writing the response
+		http.Error(writer, "An unexpected error occurred", http.StatusInternalServerError)
+		// Write response
+
+		fmt.Fprintf(writer, "Hello, you've requested: %s\n", request.URL.Path)
+	})
+
+	http.HandleFunc("/api-3", func(writer http.ResponseWriter, request *http.Request) {
+		log.Info("Calling API 3");
+
 		// Write response
 		fmt.Fprintf(writer, "Hello, you've requested: %s\n", request.URL.Path)
 	})
