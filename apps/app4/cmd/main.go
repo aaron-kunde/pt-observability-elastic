ed0e888ca66b6aa26d7069b97f94068cfb9739fc
Aaron Kunde <aaron.kunde@adesso.de>
2024-01-03 20:04:44 +0100

    Add basic prometheus metrics

--- apps/app4/cmd/main.go
+++ apps/app4/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
 	log "pt.observability.elastic/app4/internal/logging"
 	"pt.observability.elastic/app4/internal/rest"
@@ -11,6 +12,9 @@ var port = 8084
 
 func main()  {
 	log.Info("Starting app4. Listening on port: ", port)
+
+	http.Handle("/actuator/prometheus", promhttp.Handler())
+
 	rest.RegisterApiHandler()
 
 	http.ListenAndServe(":" + strconv.Itoa(port), nil)
