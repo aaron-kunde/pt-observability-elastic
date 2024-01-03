a036fdfebdd2feffab8b80a7d02ed57c773c0679
Aaron Kunde <aaron.kunde@adesso.de>
2024-01-03 23:06:21 +0100

    Add some custom metrics

--- apps/app4/internal/rest/AppRestController.go
+++ apps/app4/internal/rest/AppRestController.go
@@ -3,13 +3,34 @@ package rest
 import (
 	"errors"
 	"fmt"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
 	"net/http"
 	log "pt.observability.elastic/app4/internal/logging"
 )
 
+var (
+	namespace = "app4"
+	api1Counter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "api_1_counter",
+		Namespace: namespace,
+		ConstLabels: prometheus.Labels{"it_1": "it-2"},
+	})
+	api2Counter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "api_2_counter",
+		Namespace: namespace,
+		ConstLabels: prometheus.Labels{"it_1": "it-2"},
+	})
+	api3Counter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "api_3_counter",
+		Namespace: namespace,
+		ConstLabels: prometheus.Labels{"it_1": "it-2"},
+	})
+)
 func RegisterApiHandler() {
 	http.HandleFunc("/api-1", func(writer http.ResponseWriter, request *http.Request) {
 		log.Info("Calling API 1");
+		api1Counter.Inc()
 
 		// Write response
 		fmt.Fprintf(writer, "Hello, you've requested: %s\n", request.URL.Path)
@@ -17,6 +38,8 @@ func RegisterApiHandler() {
 
 	http.HandleFunc("/api-2", func(writer http.ResponseWriter, request *http.Request) {
 		log.Info("Calling API 2");
+		api2Counter.Inc()
+
 		// Must be called, before writing a diffferent response
 		err := errors.New("An unexpected error occurred")
 		http.Error(writer, err.Error(), http.StatusInternalServerError)
@@ -25,6 +48,7 @@ func RegisterApiHandler() {
 
 	http.HandleFunc("/api-3", func(writer http.ResponseWriter, request *http.Request) {
 		log.Info("Calling API 3");
+		api3Counter.Inc()
 
 		// Write response
 		fmt.Fprintf(writer, "Hello, you've requested: %s\n", request.URL.Path)
